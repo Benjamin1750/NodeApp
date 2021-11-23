3430cbe4f00ff90732225c2c57888bead54251ad
Benjamin1750 <91260527+Benjamin1750@users.noreply.github.com>
2021-11-23 17:26:23 +0100

    Update NodeApp.js

--- NodeApp.js
+++ NodeApp.js
@@ -2,4 +2,4 @@ var http = require('http');
 http.createServer(function (req, res){
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Hello World bis!\n');
-}).listen(1337,'127.0.0.1')
+}).listen(1337,'192.168.5.1337')
